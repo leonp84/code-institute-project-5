9e42b6eebd56e4eb6f50afed8325a2a491f191c0
Leon Potgieter <hexvallei@gmail.com>
2024-06-19 13:16:34 +0000

    design layout of product details page with data fetching, to resolve #3

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('all_products/', views.all_products, name='all_products'),
+    path('product_detail/<int:product_id>',
+         views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -3,8 +3,12 @@ from .models import Product
 
 
 def all_products(request):
-
     products = Product.objects.all()
-
     context = {'products': products}
     return render(request, 'product/all_products.html', context)
+
+
+def product_detail(request, product_id):
+    product = Product.objects.filter(pk=product_id).first()
+    context = {'product': product}
+    return render(request, 'product/product_detail.html', context)
