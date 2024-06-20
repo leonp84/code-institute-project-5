0f9624f7303198e251c0776468d34b60f395e27d
Leon Potgieter <hexvallei@gmail.com>
2024-06-20 15:41:47 +0000

    add privacy poliy + terms and conditions pages with footer links, to resolve #9

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('terms_and_conditions/', 
+         views.terms_and_conditions, name='terms_and_conditions'),
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,15 @@ def about(request):
     template = 'main/about.html'
     context = {}
     return render(request, template, context)
+
+
+def privacy_policy(request):
+    template = 'main/privacy_policy.html'
+    context = {}
+    return render(request, template, context)
+
+
+def terms_and_conditions(request):
+    template = 'main/terms_and_conditions.html'
+    context = {}
+    return render(request, template, context)
