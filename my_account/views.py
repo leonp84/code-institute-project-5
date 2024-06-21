105c35164169bb44d7ddbc0b600f87a103302a1d
Leon Potgieter <hexvallei@gmail.com>
2024-06-21 18:54:44 +0000

    set up login and logout functionality through allauth, to resolve #15

--- my_account/views.py
+++ my_account/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, HttpResponse # noqa
 
 
 def my_account(request):
-    return HttpResponse('my_account')
+    template = 'my_account/my_account.html'
+    context = {}
+    return render(request, template, context)
