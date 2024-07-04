77e3202ef3bf950ddaeaa1acb1a3ec0321e85994
Leon Potgieter <hexvallei@gmail.com>
2024-07-04 18:02:51 +0000

    add url paths for robots.txt and sitemap.xml

--- heritage/urls.py
+++ heritage/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,12 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('product/', include('product.urls')),
     path('checkout/', include('checkout.urls')),
+    # Make robots.txt accessible as root file
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain")),
+    # Make sitemaps.xml accessible as root file
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml",
+                                             content_type="text/xml")),
 ]
 
 # Setting to serve Django Media files in Development environment
